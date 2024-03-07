4d9c1324e072da789cad80416aa47c809b0670d9
unknown <chavarotman123@gmail.com>
2024-03-07 10:13:51 +0200

    commit 5

--- hostages/urls.py
+++ hostages/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.new_page),
+    path('', views.index),
+    path('en', views.index),
+    path('he', views.index),
     path('new', views.new_page)
 ]
--- hostages/views.py
+++ hostages/views.py
@@ -1,4 +1,26 @@
-from django.http import HttpResponse
+from django.http import  HttpResponse
+from django.shortcuts import render
+from .models import Hostage
+
+import os
+import sys
+
+def index(request):
+    hostages = list(Hostage.objects.all())
+    hostages = hostages+hostages
+    hostages = hostages + hostages
+    hostages = hostages + hostages
+    isHebrew = True
+    if request.path == '/home/en':
+        isHebrew=False
+        for val in hostages:
+            if val.english_name != '':
+                val.name = val.english_name
+            if val.english_info != '':
+                val.info = val.english_info
+    return render(request,
+                   'index.html',
+                   {'hostages': hostages, 'isHebrew': isHebrew})
 
 
 def new_page(request):
