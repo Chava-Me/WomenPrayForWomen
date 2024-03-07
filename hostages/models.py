5e5537b80317bab0f6948e1393c594a994475f8f
unknown <chavarotman123@gmail.com>
2024-03-07 10:06:51 +0200

    commit 4

--- hostages/models.py
+++ hostages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Hostage(models.Model):
+    name = models.fields.CharField(max_length=225)
+    pray_name = models.fields.CharField(default='',max_length=225)
+    info = models.fields.CharField(max_length=2010)
+    english_name = models.fields.CharField(default='',max_length=225)
+    english_info = models.fields.CharField(default='',max_length=2010)
+    img = models.fields.CharField(max_length=2010)
+    url = models.fields.CharField(max_length=2010)
